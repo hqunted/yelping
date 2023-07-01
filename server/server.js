7c92a637b7d51f7627e6485e8176d603c9cc626e
hqunted <berkekirci@gmail.com>
2023-07-01 11:14:36 +0200

    sadasd

---
+++ server/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get('/getRestaurants')
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is up and listening on port ${port}`);
+});
