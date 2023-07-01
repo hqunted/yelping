623c044eb65408c94f4a2aeeeca9f2c836bcf378
hqunted <berkekirci@gmail.com>
2023-07-01 11:37:41 +0200

    feat: configure server.js & npm

--- server/server.js
+++ server/server.js
@@ -3,8 +3,12 @@ const express = require("express");
 
 const app = express();
 
-app.get('/getRestaurants')
-
+app.get("/getRestaurants", (req, res) => {
+  res.json({
+    status: "success",
+    restaurant: "mcdonalds",
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
